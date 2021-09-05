31673d22c7fc284b7ad0358bf38dd25b9f17c69a
Alberto Zanovello <zanovello2002@gmail.com>
2021-09-05 09:56:00 +0200

    dd

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module alberto
 go 1.16
 
 require (
-	github.com/hajimehoshi/ebiten v1.12.12
 	github.com/hajimehoshi/ebiten/v2 v2.1.5
 )
---
+++ src/game.go
@@ -0,0 +1,49 @@
+package src
+
+import (
+	"alberto/src/res"
+	"fmt"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	screenWidth  = 240
+	screenHeight = 240
+)
+
+type Game struct {
+	back  [10][10]int
+	front [10][10]int
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw each tile with each DrawImage call.
+	// As the source images of all DrawImage calls are always same,
+	// this rendering is done very efficiently.
+	// For more detail, see https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Image.DrawImage
+
+	screen.DrawImage(&res.Player[0], &ebiten.DrawImageOptions{})
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	g := &Game{}
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Tiles (Ebiten Demo)")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
