e2dc24c89ba320def88d8a856239d257f10091fb
Alberto Zanovello <zanovello2002@gmail.com>
2021-10-30 14:42:11 +0200

    Merge branch 'master' of https://github.com/ZanovelloAlberto/EbitenGame

--- core/main.go
+++ core/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "core/game"
-
 func main() {
-	game.Start()
+	FibonacciLoop(46)
+}
+func FibonacciLoop(n int) int {
+	f := make([]int, n+1, n+2)
+	if n < 2 {
+		f = f[0:2]
+	}
+	f[0] = 0
+	f[1] = 1
+	for i := 2; i <= n; i++ {
+		f[i] = f[i-1] + f[i-2]
+	}
+	return f[n]
 }
