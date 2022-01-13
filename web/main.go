2692a2fbcbe4f3b3f7df151db5e4bb1b685729a1
AlbertoZanovello <zanovello2002@gmail.com>
2022-01-13 23:10:41 +0100

    df

--- web/main.go
+++ web/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"log"
-
-	twenty48 "github.com/ZanovelloAlberto/EbitenGame/2048"
-	"github.com/hajimehoshi/ebiten/v2"
-)
-
 func main() {
-	game, err := twenty48.NewGame()
-	if err != nil {
-		log.Fatal(err)
-	}
-	ebiten.SetWindowSize(twenty48.ScreenWidth, twenty48.ScreenHeight)
-	ebiten.SetWindowTitle("2048 (Ebiten Demo)")
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
+
 }
